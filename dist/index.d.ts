af232d05762ddd04647cd1b65a8e5691989b8315
Kyoudan <fogoste@gmail.com>
2023-06-19 10:20:29 -0300

    fix: build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,31 +1,26 @@
-type TypeVerifyInputDTO =
-  | "email"
-  | "phone"
-  | "uuid"
-  | "cpf"
-  | "string"
-  | "number"
-  | TypeVerifyInputArray;
-type TypeVerifyInputArray = Array<TypeVerifyInput | TypeVerifyInputArray>;
-type TypeVerifyInput = "email" | "phone" | "uuid" | "cpf" | "string" | "number";
+type FieldType = {
+    field: "email" | "phone" | "cpf" | "uuid";
+    format?: "string" | "number";
+};
+type TypeVerifyInputDTO = "email" | "phone" | "uuid" | "cpf" | "string" | "number" | FieldType;
 interface VerifyInputDTO {
-  value: string | number;
-  type: TypeVerifyInputDTO;
-  max?: number;
-  min?: number;
+    value: string | number;
+    type: TypeVerifyInputDTO;
+    max?: number;
+    min?: number;
 }
 interface ResponseVerifyDTO {
-  success: boolean;
-  message: string;
-  type: TypeVerifyInputDTO;
+    success: boolean;
+    message: string;
+    type: TypeVerifyInputDTO;
 }
 
 interface MethodsInput {
-  verify: (input: VerifyInputDTO) => ResponseVerifyDTO;
+    verify: (input: VerifyInputDTO) => ResponseVerifyDTO;
 }
 
 declare class Axval implements MethodsInput {
-  verify: (input: VerifyInputDTO) => ResponseVerifyDTO;
+    verify: (input: VerifyInputDTO) => ResponseVerifyDTO;
 }
 declare const _default: Axval;
 
